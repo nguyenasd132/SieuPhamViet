b5e0beee9bc8e65b10c45e4c3c2ac9d58e462199
nguyenasd132 <gatogamer336@gmail.com>
2024-12-03 21:29:03 +0700

    update slideshow

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+const listImage = document.querySelector('.list-images');
+const imgs = document.querySelectorAll('.list-images img');
+const length = imgs.length;
+let current = 0;
+
+function showCurrentImage() {
+    imgs.forEach(img => {
+        img.style.display = 'none';  
+    });
+    imgs[current].style.display = 'block';  
+}
+setInterval(() => {
+    current++; 
+    if (current === length) {
+        current = 0; 
+    }
+    showCurrentImage();
+}, 3000); 
+
+showCurrentImage();
+
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: false,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 2
+        },
+        768: {
+            slidesPerView: 3
+        },
+        1024: {
+            slidesPerView: 4
+        }
+    }
+});
