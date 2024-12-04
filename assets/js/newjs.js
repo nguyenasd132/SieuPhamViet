c783d1ff07bc9075a950f04315084d44fe6bc909
Tuans2212 <psmotvn@gmail.com>
2024-12-04 13:40:29 +0700

    update dangki dangnhap

---
+++ assets/js/newjs.js
@@ -0,0 +1,10 @@
+
+function click(){
+    var x = document.getElementById("password");
+    if(x.type =='password'){
+        x.type= "text";
+    }
+    else{
+        x.type = "passwword";
+    }
+}
