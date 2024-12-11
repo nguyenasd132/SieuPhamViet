f388cb2b249f98a99cf877522a73efcb2bd74bd9
nguyenasd132 <gatogamer336@gmail.com>
2024-12-11 13:48:45 +0700

    update v1.4.3

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,25 @@ const imgs = document.querySelectorAll('.list-images img');
 const length = imgs.length;
 let current = 0;
 
+function showCurrentImage() {
+    imgs.forEach(img => {
+        img.style.display = 'none';
+    });
+    imgs[current].style.display = 'block';
+}
+setInterval(() => {
+    current++;
+    if (current === length) {
+        current = 0;
+    }
+    showCurrentImage();
+}, 3000);
+
+showCurrentImage();
+
+
+
+
 const swiper = new Swiper('.spSwiper', {
     // Optional parameters
     loop: false,
