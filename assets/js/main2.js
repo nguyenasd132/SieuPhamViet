f54a728344a4f2549f7ce45bfb79ccc269e9272c
nguyenasd132 <gatogamer336@gmail.com>
2024-12-14 00:23:29 +0700

    update v1.6

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -19,17 +19,30 @@ window.onscroll = function() {
     prevScrollpos = currentScrollPos;
 }
 var xemthem = document.querySelector('#xemthem');
+var containerSP = document.getElementById("body-2");
 console.log(xemthem);
 let soSP = 8;
+let chieucao = containerSP.offsetHeight;
+let chieucao2 = document.querySelector('.danhMucItem .sale-container-1 .sp-card');
+let footer = document.getElementById("footer");
+console.log(chieucao2.offsetHeight);
+let k = 0;
 
-xemthem.onclick = ()=>{
+function loadmore(){
+    k++;
+    let chieucao3 = chieucao + 412*k;
     let tongSP = [...document.querySelectorAll('.sale-container-1 .sale-slide-show .sp-card')];
-    console.log(tongSP[1]);
     for( var i = soSP; i < soSP + 4; i++){
-        tongSP[i].styles.display = 'inline-block';
+        tongSP[i].style.display = 'inline-block';
     }
     soSP += 4;
-    if(soSP >= tongSP.length){
+    containerSP.style.height = chieucao3 + "px";
+    footer.style.marginTop = 412*k + "px";
+    xemthem.style.top = 412*(k+1) + "px";
+
+    
+    if( soSP >= tongSP.length - 2){
         xemthem.style.display = 'none';
     }
+
 }
